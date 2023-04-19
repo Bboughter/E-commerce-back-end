1277bdc06b73100e741a90b675ef1f19e55e3d1b
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-18 20:32:07 -0700

    tag route mostly complete other than get by id

--- models/Tag.js
+++ models/Tag.js
@@ -2,7 +2,7 @@ const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection.js');
 
-class Tag extends Model {}
+class Tag extends Model { }
 
 Tag.init(
   {
@@ -11,11 +11,11 @@ Tag.init(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
+    },
+    tag_name: {
+      type: DataTypes.STRING,
+    },
   },
-  tag_name: {
-    type: DataTypes.STRING,
-  },
-},
   {
     sequelize,
     timestamps: false,
