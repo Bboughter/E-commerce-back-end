cb90ff4f67997df2235bc455c8e223550b6fe38c
Brittany Boughter <brittany.wood0308@yahoo.com>
2023-04-19 09:58:57 -0700

    fixed issue with tags get route by id

--- models/index.js
+++ models/index.js
@@ -19,7 +19,7 @@ Product.belongsToMany(Tag, {
     foreignKey: 'product_id',
     unique: false,
   },
-  as: 'product_tags'
+
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
@@ -27,7 +27,7 @@ Tag.belongsToMany(Product, {
     model: ProductTag,
     unique: false,
   },
-  as: 'product_tags',
+  
 });
 
 module.exports = {
--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, through: ProductTag, as: 'product_tags' }],
+      include: [{ model: Product, through: ProductTag }],
     });
     res.status(200).json(tagData);
   }catch (err) {
@@ -18,8 +18,8 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
-    const tagData = Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: ProductTag, as: 'product_tags' }],
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag }],
     });
     if (!tagData) {
       res.status(404).json({ message: 'No tag found with this id!' });
